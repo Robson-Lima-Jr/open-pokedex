fa4f96de9bb44f42ed870243c5c16e44a6925026
Rob Jr <robsonjuniorsn@gmail.com>
2025-12-30 19:36:38 -0300

    ...

--- src/app/components/Topo/index.tsx
+++ src/app/components/Topo/index.tsx
@@ -1,31 +1,34 @@
 "use client";
-import style from "./Topo.module.css";
+import styles from "./Topo.module.css";
 import Link from "next/link";
+import Image from "next/image";
 
-export default function Topo () {
-    return(
-        <header className={style.fundo_topo}>
-            <div className={style.logo}>
-                <span>Pokédex</span>
+export default function Topo() {
+    return (
+        <header className={styles.fundo_topo}>
+            <div className={styles.container_base}>
+                <div className={styles.logo}>
+                    <Image src="/pokeball-logo.png" alt="logo" width={50} height={50} className={styles.topo} />
+                </div>
+
+                <nav className={styles.nav}>
+                    <Link href="/" className={styles.link_nav}>Home</Link>
+                    <Link href="/pokedex" className={styles.link_nav}>Pokédex</Link>
+                    <Link href="/games" className={styles.link_nav}>Games</Link>
+                </nav>
+
+                <div className={styles.container_tema}>
+                    <select aria-label="Tema" className={styles.select_btn}>
+                        <option value="red">Red</option>
+                        <option value="blue">Blue</option>
+                        <option value="yellow">Yellow</option>
+                    </select>
+                </div>
+
+                <button aria-label="Mudar modo" className={styles.select_btn}>
+                    🌗
+                </button>
             </div>
-
-            <nav className={style.nav}>
-                <Link href="/">Home</Link>
-                <Link href="/pokedex">Pokédex</Link>
-                <Link href="/games">Games</Link>
-            </nav>
-
-            <div className={style.container_tema}>
-                <select aria-label="Tema">
-                    <option value="red">Red</option>
-                    <option value="blue">Blue</option>
-                    <option value="yellow">Yellow</option>
-                </select>
-            </div>
-
-            <button aria-label="Mudar modo">
-                🌗
-            </button>
         </header>
     )
 }
