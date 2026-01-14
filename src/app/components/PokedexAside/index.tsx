f601a0876112f8af556d06100b02d8ba3039826c
Rob Jr <robsonjuniorsn@gmail.com>
2026-01-14 19:03:06 -0300

    atualização aside

--- src/app/components/PokedexAside/index.tsx
+++ src/app/components/PokedexAside/index.tsx
@@ -1,9 +1,11 @@
 import styles from "./PokedexAside.module.css";
+import pokemonTypes from "@/app/data/pokemonTypes";
+import regions from "@/app/data/regions";
 
 export default function PokedexAside() {
     return (
         <aside className={styles.container_aside}>
-            <h2 className={styles.h2_aside}><span>◓</span>Filtros</h2>
+            <h2 className={styles.h2_aside}>◓ Filtros</h2>
 
             {/* Busca por texto */}
             <div className={styles.aside_bloco}>
@@ -23,6 +25,14 @@ export default function PokedexAside() {
                 <button className={styles.aside_botao}>
                     <span>▶</span> Tipos
                 </button>
+
+                <ul className={styles.aside_ul}>
+                    {pokemonTypes.map((type) => (
+                        <li key={type.id} className={styles.aside_li}> 
+                            <span>◓</span> {type.namePt} ({type.nameEn})
+                        </li>
+                    ))}
+                </ul>
             </div>
 
             {/* Região */}
@@ -30,6 +40,14 @@ export default function PokedexAside() {
                 <button className={styles.aside_botao}>
                     <span>▶</span> Regiões
                 </button>
+
+                <ul className={styles.aside_ul}>
+                    {regions.map ((region) => (
+                        <li key={region.id} className={styles.aside_li}>
+                            <span>◓</span> {region.namePt}
+                        </li>
+                    ))}
+                </ul>
             </div>
         </aside>
     )
