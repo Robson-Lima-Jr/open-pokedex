02f54cf27ef7afac9ea6a110f8ffe78ad6e9056e
Rob Jr <robsonjuniorsn@gmail.com>
2026-01-15 17:38:33 -0300

    editando aside

--- src/app/components/PokedexAside/index.tsx
+++ src/app/components/PokedexAside/index.tsx
@@ -5,7 +5,7 @@ import regions from "@/app/data/regions";
 export default function PokedexAside() {
     return (
         <aside className={styles.container_aside}>
-            <h2 className={styles.h2_aside}>◓ Filtros</h2>
+            <h2 className={styles.h2_aside}><span>◓</span> Filtros</h2>
 
             {/* Busca por texto */}
             <div className={styles.aside_bloco}>
--- src/app/pokedex/page.tsx
+++ src/app/pokedex/page.tsx
@@ -3,24 +3,26 @@ import PokedexAside from "../components/PokedexAside";
 
 export default function pokedex() {
     return (
-        <main className="container_base">
-            <div className="titulo_area">
-                <h1 className="titulo_h1">
-                    <span className="h1_icone">◓</span>
+        <main >
+            <div className={styles.layout_pokedex}>
+                <PokedexAside />
 
-                    <span>Pokédex</span>
-                </h1>
+                <div className={`container_base ${styles.conteudo_dex}`}>
+                    <div className="titulo_area">
+                        <h1 className="titulo_h1">
+                            <span className="h1_icone">◓</span>
+                    
+                            <span>Pokédex</span>
+                        </h1>
 
-                <p className="paragrafo_h1">Database</p>
+                        <p className="paragrafo_h1">Database</p>
+                    </div>
+
+                    <section className={`section_main ${styles.container_pokedex}`}>
+                        <h2 className="subtitulo_h2">▶ Nacional Dex</h2>
+                    </section>
+                </div>
             </div>
-
-            <PokedexAside />
-
-            <section className={`section_main ${styles.container_pokedex}`}>
-                <h2 className="subtitulo_h2">▶ Nacional Dex</h2>
-
-
-            </section>
         </main>
     )
 }
