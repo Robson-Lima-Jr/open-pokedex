bf673dfab324b2b27f7eae6f1d76c9f41e071353
Rob Jr <robsonjuniorsn@gmail.com>
2026-01-17 16:02:23 -0300

    criação de cards base, visual semi-aberto

---
+++ src/app/components/PokemonCard/index.tsx
@@ -0,0 +1,46 @@
+import styles from "./PokemonCard.module.css";
+import Image from "next/image";
+
+export default function PokemonCard() {
+    return (
+        <article className={styles.card_container}>
+            <div className={styles.card_cima}>
+                <div className={styles.sprite_area}>
+                    <Image src="/pidgeot.png" width={80} height={80} alt="pidgeot" className={styles.image}/>
+
+                    <span className={styles.dex_num}>#016</span>
+                </div>
+
+                <div className={styles.header_poke}>
+                    <h2 className={styles.nome}>Pidgeot</h2>
+
+                    <div className={styles.tipos_area}>
+                        <p className={styles.tipos_desc}>Tipo <span>1</span>/</p>
+                        <p className={styles.tipos}>Normal</p>
+                    </div>
+
+                    <div className={styles.tipos_area}>
+                        <p className={styles.tipos_desc}>Tipo <span>2</span>/</p>
+                        <p className={styles.tipos}>Voador</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className={styles.stats_area}>
+                <ul className={styles.stats}>
+                    <li className={styles.stats_li}><span>HP</span><strong>200</strong></li>
+
+                    <li className={styles.stats_li}><span>ATTACK</span><strong>135</strong></li>
+
+                    <li className={styles.stats_li}><span>DEFENSE</span><strong>157</strong></li>
+
+                    <li className={styles.stats_li}><span>SP.ATTACK</span><strong>121</strong></li>
+
+                    <li className={styles.stats_li}><span>SP.DEFENSE</span><strong>121</strong></li>
+
+                    <li className={styles.stats_li}><span>SPEED</span><strong>150</strong></li>
+                </ul>
+            </div>
+        </article>
+    )
+}
