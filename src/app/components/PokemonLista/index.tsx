ff3b2531706104dc70729e2c2e8526891095e746
Rob Jr <robsonjuniorsn@gmail.com>
2026-02-24 18:24:19 -0300

    inicio da implementação da api, em cards e listas

--- src/app/components/PokemonLista/index.tsx
+++ src/app/components/PokemonLista/index.tsx
@@ -2,19 +2,35 @@ import { IconePokeball } from "../icons/Icons";
 import styles from "./PokemonLista.module.css";
 import Image from "next/image";
 
-export default function PokemonLista() {
+interface PokemonCardProps {
+    pokemon: {
+        id: number;
+        name: string;
+        sprites: {
+            front_default: string;
+        };
+        types: {
+            slot: number;
+            type: {
+                name: string;
+            };
+        }[];
+    };
+}
+
+export default function PokemonLista({pokemon}: PokemonCardProps) {
     return (
         <article className={styles.lista_container}>
             <IconePokeball className={styles.icone_lista} />
 
             <div className={styles.area_info}>
-                <span className={styles.dex_num}>0016</span>
+                <span className={styles.dex_num}>#{pokemon.id.toString().padStart(4, "0")}</span>
 
                 <div className={styles.sprite_area}>
-                    <Image src="/pidgeot.png" width={50} height={50} alt="pidgeot" className={styles.image} />
+                    <Image src={pokemon.sprites.front_default} width={60} height={60} alt="pidgeot" className={styles.image} />
                 </div>
 
-                <p className={styles.lista_nome}>Feraligatrz</p>
+                <p className={styles.lista_nome}>{pokemon.name}</p>
 
                 <div className={styles.tipos_area}>
                     <span>Normal</span>
