ae0c0e2a6082c353ac0c2206e30177165bdae9a0
Rob Jr <robsonjuniorsn@gmail.com>
2026-05-07 18:26:50 -0300

    pagina aberta inicio de inclusao de dados

--- src/app/pokemon/[name]/page.tsx
+++ src/app/pokemon/[name]/page.tsx
@@ -3,16 +3,72 @@ import Link from "next/link";
 import { IconeSeta, IconeLink, IconeBaixo } from "../../components/icons/Icons";
 import { StatsGroup } from "@/app/components/Stats/StatsGroup";
 import Image from "next/image";
+import { stat } from "fs";
 
-export default function PokemonPage({ params }: { params: { id: string } }) {
-    const mockStats = {
-        hp: 95,
-        atk: 20,
-        def: 65,
-        spa: 131,
-        spd: 55,
-        spe: 151,
+interface PokemonData {
+    id: number;
+    name: string;
+
+    height: number;
+    weight: number;
+
+    sprites: {
+        other: {
+            "official-artwork": {
+                front_default: string;
+            };
+        };
     };
+
+    types: {
+        type: {
+            name: string;
+        };
+    }[];
+
+    abilities: {
+        ability: {
+            name: string;
+        };
+
+        is_hidden: boolean;
+    }[];
+
+    stats: {
+        base_stat: number;
+
+        stat: {
+            name: string;
+        };
+    }[];
+}
+
+export default async function PokemonPage({
+    params,
+}: {
+    params: Promise<{ name: string }>;
+}) {
+
+    const { name } = await params;
+
+    const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`
+    );
+
+    if (!response.ok) {
+        throw new Error("Pokémon não encontrado");
+    }
+
+    const pokemon: PokemonData = await response.json();
+
+    const stats = {
+        hp: pokemon.stats.find((stat) => stat.stat.name === "hp")?.base_stat || 0,
+        atk: pokemon.stats.find((stat) => stat.stat.name === "attack")?.base_stat || 0,
+        def: pokemon.stats.find((stat) => stat.stat.name === "defense")?.base_stat || 0,
+        spa: pokemon.stats.find((stat) => stat.stat.name === "special-attack")?.base_stat || 0,
+        spd: pokemon.stats.find((stat) => stat.stat.name === "special-defense")?.base_stat || 0,
+        spe: pokemon.stats.find((stat) => stat.stat.name === "speed")?.base_stat || 0
+    }
 
     return (
         <div className={styles.center}>
@@ -23,21 +79,21 @@ export default function PokemonPage({ params }: { params: { id: string } }) {
                     <div className={styles.nav_pokemon}>
                         <Link href="#" className={styles.link_pokemon}>
                             <IconeLink className={styles.link_icon} />
-                            <span>#0017</span> Pidgeotto
+                            <span>#{pokemon.id - 1}</span> {pokemon.name}
                         </Link>
 
                         <Link href="#" className={styles.link_pokemon}>
-                            <span>#0019</span> Rattata
+                            <span>#{pokemon.id + 1}</span> {pokemon.name}
                             <IconeSeta className={styles.link_icon} />
                         </Link>
                     </div>
 
-                    <h1 className={styles.h1_pokemon}>Pidgeot</h1>
+                    <h1 className={styles.h1_pokemon}>{pokemon.name}</h1>
                 </section>
 
                 {/* imagem do pokemon */}
                 <section className={styles.imagem_area}>
-                    <Image src="/018.png" width={400} height={400} alt="Pokémon" className={styles.imagem_pokemon}></Image>
+                    <Image src={pokemon.sprites.other["official-artwork"].front_default} width={400} height={400} alt="Pokémon" className={styles.imagem_pokemon}></Image>
                 </section>
 
                 {/* dados pokemon */}
@@ -46,13 +102,13 @@ export default function PokemonPage({ params }: { params: { id: string } }) {
 
                     <div>
                         <div className={styles.fundo_dados}>
-                            <p className={styles.item_dados}>Nº Pokédex: <span className={styles.valores_dados}>#0018</span></p>
+                            <p className={styles.item_dados}>Nº Pokédex: <span className={styles.valores_dados}>#{pokemon.id}</span></p>
 
-                            <p className={styles.item_dados}>Altura: <span className={styles.valores_dados}>1.50 m</span></p>
+                            <p className={styles.item_dados}>Altura: <span className={styles.valores_dados}>{pokemon.height} m</span></p>
 
-                            <p className={styles.item_dados}>Peso: <span className={styles.valores_dados}>30 kg</span></p>
+                            <p className={styles.item_dados}>Peso: <span className={styles.valores_dados}>{pokemon.weight} kg</span></p>
 
-                            <p className={styles.item_dados}>Gênero: <span className={styles.valores_dados}>Desconhecido</span></p>
+                            <p className={styles.item_dados}>Gênero: <span className={styles.valores_dados}></span></p>
 
                             <p className={styles.item_dados}>Categoria: <span className={styles.valores_dados}>Bola</span></p>
 
@@ -70,9 +126,15 @@ export default function PokemonPage({ params }: { params: { id: string } }) {
                     <h2 className={styles.h2_descricao}>Tipo</h2>
 
                     <div className={styles.container_tipos}>
-                        <span className={styles.tipo_pokemon} data-type="normal">Normal</span>
-
-                        <span className={styles.tipo_pokemon} data-type="flying">Flying</span>
+                        {pokemon.types.map((type) => (
+                            <span
+                                key={type.type.name}
+                                className={styles.tipo_pokemon}
+                                data-type={type.type.name}
+                            >
+                                {type.type.name}
+                            </span>
+                        ))}
                     </div>
 
                     {/* fraquezas */}
@@ -106,7 +168,7 @@ export default function PokemonPage({ params }: { params: { id: string } }) {
                     <h2 className={styles.h2_descricao}>Stats</h2>
 
 
-                    <StatsGroup stats={mockStats} />
+                    <StatsGroup stats={stats} />
                 </section>
 
                 {/* linha evolutiva */}
